83c12ae82fc40d901abd30cdd2297d52a4027001
liya2133 <liyasebastian2108@gmail.com>
2026-07-13 22:11:13 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,22 @@ import './App.css'
 import AddCars from './components/addCars'
 import ViewCars from './components/ViewCars'
 import Nav from './components/Nav'
+import Home from './components/Home'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <AddCars />
-        <ViewCars />
-        <Nav />
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+        <Route path='/add' element={<AddCars/>}/>
+        <Route path='/view' element={<ViewCars/>}/>
+
+     </Routes>
+      </BrowserRouter>
     </>
   )
 }
